476bf3551465116adc5913bf9639713f96bfd5de
Irina Poposki <ninapoposki6@gmail.com>
2024-05-30 11:48:52 +0200

    try products

--- Backend/WebShopAppREST/src/main/java/services/ProductService.java
+++ Backend/WebShopAppREST/src/main/java/services/ProductService.java
@@ -13,19 +13,31 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import beans.Product;
+import dao.ProductDAO;
 
 
 @Path("/products")
 public class ProductService {
+	@Context
+	ServletContext ctx;
 	
 	public ProductService() {
+	}
+	
+	@PostConstruct
+	public void init() {
+		if(ctx.getAttribute("productDAO")==null) {
+			String ContextPath=ctx.getRealPath("");
+			ctx.setAttribute("productDAO", new ProductDAO(ContextPath));
+		}
 	}
 	
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<Product> getProducts() {
-		return null;
+		ProductDAO dao=(ProductDAO) ctx.getAttribute("productDAO");
+		return dao.findAll();
 	}
 	
 }
